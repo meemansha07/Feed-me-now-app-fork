dab68bc7e0d75767942df7499888dc5a1ea194c0
Meemansha Singh <meemanshasingh77@gmail.com>
2024-03-11 10:23:35 +0530

    add files

---
+++ mernapp/src/screens/Home.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import Navbar from '../components/Navbar';
+import Footer from '../components/Footer';
+import Carousel from '../components/Carousel';
+import Card from '../components/Card';
+
+export default function Home() {
+    return (
+        <div>
+            <div><Navbar /></div>
+            <div><Carousel /></div>
+            <div className='m-3'>
+                <Card />
+                <Card />
+                <Card />
+                <Card />
+                
+                </div>
+            <div><Footer /></div>
+        </div>
+    );
+}

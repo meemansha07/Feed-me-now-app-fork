2a11563fa1a4ab79d010eb933456bb0dc79195fa
Meemansha Singh <meemanshasingh77@gmail.com>
2024-03-13 10:09:03 +0530

    change

--- mernapp/backend/db.js
+++ mernapp/backend/db.js
@@ -1,16 +1,18 @@
 const mongoose = require('mongoose');
-const mongoURI = 'mongodb+srv://feedMeNow:mern1234@cluster0.p72zczp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+const mongoURI = 'mongodb+srv://meemanshasingh77:mern123@cluster0.sk8mvcz.mongodb.net/gofoodmern?retryWrites=true&w=majority&appName=Cluster0';
 
-const mongoDB =() =>{
-    mongoose.connect(mongoURI, {useNewUrlParser: true, useUnifiedTopology: true})
-    .then(() => {
+const mongoDB = async () => { // Define mongoDB function as async
+    try {
+        await mongoose.connect(mongoURI); // Wait for connection to be established
         console.log('connected');
-    })
-    .catch((err) => {
-        console.log('error connecting to mongoDB:', err);
-    });
+        const fetched_data = await mongoose.connection.db.collection("food_items");
+        fetched_data.find({}).toArray( function(err,data) {
+            if(err) console.log(err);
+            else console.log(data)
+        });
+    } catch (err) {
+        console.log('error connecting to MongoDB:', err);
+    }
 }
-    
 
 module.exports = mongoDB;
-
